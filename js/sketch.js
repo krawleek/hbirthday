6cd0041929c8451ed49fb3228bb37965fa3ce055
Elena <lenusya.solodovnikova@gmail.com>
2020-05-23 23:03:09 +0300

    sketch upd

--- js/sketch.js
+++ js/sketch.js
@@ -1,13 +1,5 @@
-let img;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bg = loadImage('img/bg.svg')
-  background(bg)
-  img = loadImage('img/writing.png')
-  image(img,0,height/2,img.width/2,img.height/2);
-
-
 }
 
 function draw() {
