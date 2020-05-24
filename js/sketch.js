2357a05385f03b6364db8de2d7026e2edc49bfcb
Elena <lenusya.solodovnikova@gmail.com>
2020-05-24 13:55:25 +0300

    colored line

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,19 @@
+let col={
+  r:0,
+  g:0,
+  b:0,
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  
-  stroke(0);
+  col.r=random(0,255);
+  col.g=random(0,255);
+  col.b=random(0,255);
+
+  stroke(col.r, col.g, col.b);
   strokeWeight(4);
   if (mouseIsPressed === true) {
     line(mouseX, mouseY, pmouseX, pmouseY);
